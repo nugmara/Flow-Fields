5b9b24589f5946b980f184ed225208bf5332e459
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-10 17:02:30 +0200

    Add pattern movement to the animation of the particles

--- script.js
+++ script.js
@@ -14,15 +14,29 @@ class Particle {
     this.effect = effect;
     this.x = Math.floor(Math.random() * this.effect.width);
     this.y = Math.floor(Math.random() * this.effect.height);
-    this.speedX = Math.random() * 5 -2.5;
+    this.speedX = Math.random() * 5 - 2.5;
     this.speedY = Math.random() * 5 - 2.5;
+    this.history = [{ x: this.x, y: this.y }];
+    this.maxLength = Math.floor(Math.random() * 100 + 10);
+    this.angle = 0;
   }
   draw(context) {
     context.fillRect(this.x, this.y, 10, 10);
+    context.beginPath();
+    context.moveTo(this.history[0].x, this.history[0].y);
+    for (let i = 0; i < this.history.length; i++) {
+      context.lineTo(this.history[i].x, this.history[i].y);
+    }
+    context.stroke();
   }
   update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
+    this.angle += 0.5;
+    this.x += this.speedX + Math.sin(this.angle) * 10;
+    this.y += this.speedY + Math.cos(this.angle) * 7;
+    this.history.push({ x: this.x, y: this.y });
+    if (this.history.length > this.maxLength) {
+      this.history.shift();
+    }
   }
 }
 
@@ -53,7 +67,7 @@ console.log(effect);
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  effect.render(ctx);
+//   effect.render(ctx);
   requestAnimationFrame(animate);
 }
 animate();
