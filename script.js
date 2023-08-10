7e088c3f10f5e5a16f3feaeec9306603ef913c31
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-10 16:51:38 +0200

    Add animation to the particles

--- script.js
+++ script.js
@@ -14,9 +14,15 @@ class Particle {
     this.effect = effect;
     this.x = Math.floor(Math.random() * this.effect.width);
     this.y = Math.floor(Math.random() * this.effect.height);
+    this.speedX = Math.random() * 5 -2.5;
+    this.speedY = Math.random() * 5 - 2.5;
   }
   draw(context) {
     context.fillRect(this.x, this.y, 10, 10);
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
   }
 }
 
@@ -26,21 +32,28 @@ class Effect {
     this.height = height;
     this.particles = [];
     this.numberOfParticles = 50;
-    this.init()
+    this.init();
   }
   init() {
     // create particles
     for (let i = 0; i < this.numberOfParticles; i++) {
-        this.particles.push(new Particle(this));
+      this.particles.push(new Particle(this));
     }
   }
   render(context) {
     this.particles.forEach((particle) => {
-        particle.draw(context)
-    })
+      particle.draw(context);
+      particle.update();
+    });
   }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
-effect.render(ctx)
-console.log(effect)
+console.log(effect);
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  effect.render(ctx);
+  requestAnimationFrame(animate);
+}
+animate();
