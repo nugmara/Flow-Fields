ca0115e6eed25f7fbbcf0f120a37d7e4449bd44b
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-10 16:23:38 +0200

    Create a random particle on canvas

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-const canvas = document.getElementById("canvas1")
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // canvas settings
-console.log(ctx)
-ctx.fillStyle = "white"
-ctx.strokeStyle  = "white"
-ctx.lineWidth = "100"
-ctx.lineCap = "round"
+console.log(ctx);
+ctx.fillStyle = "white";
+ctx.strokeStyle = "white";
+ctx.lineWidth = 1;
 
+class Particle {
+  constructor(effect) {
+    this.effect = effect;
+    this.x = Math.floor(Math.random() * this.effect.width);
+    this.y = Math.floor(Math.random() * this.effect.height);
+  }
+  draw(context) {
+    context.fillRect(this.x, this.y, 10, 10);
+  }
+}
 
-ctx.beginPath()
-ctx.moveTo(100, 200);
-ctx.lineTo(400, 500);
-ctx.stroke()
+class Effect {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.particles = [];
+  }
+  init() {
+    this.particles.push(new Particle(this));
+  }
+  render(context) {
+    this.particles.forEach((particle) => {
+        particle.draw(context)
+    })
+  }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+effect.init();
+effect.render(ctx)
+console.log(effect)
